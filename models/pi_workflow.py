06e61a5b18601bd8aee4d85e3ba0c651f0b5a5f6
lyt0615 <lyt20000615@163.com>
2024-11-08 18:22:43 +0800

    1108 update

--- models/pi_workflow.py
+++ models/pi_workflow.py
@@ -3,13 +3,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class LoadModel(nn.Module):
+class PIModel(nn.Module):
     def __init__(self, base_model, in_channels, classifier):
-        super(LoadModel, self).__init__()
+        super(PIModel, self).__init__()
         # self.n_classes = num_classes
         # self.fc_input_dim = fc_input_dim
         # modules = list(base_model.children())
-        new_conv_layer = nn.Conv1d(in_channels, in_channels, 5, padding=2)
+        new_conv_layer = nn.Conv1d(in_channels, in_channels, 3, padding=1)
         base_model.append(new_conv_layer)
         # modules[-1] = nn.Sequential(nn.Flatten(),
         #                             nn.Linear(in_features=self.fc_input_dim, out_features=self.n_classes))
@@ -19,9 +19,47 @@ class LoadModel(nn.Module):
         # Correlation matrix for multi-label (still used but for independent labels)
         # self.correlation = nn.Parameter(F.softmax(torch.rand(size=(self.n_classes, self.n_filters)), dim=0))
 
+    def multinomial(self, pred_softmax, target):
+        sample_cat_oh = torch.zeros(pred_softmax.shape)
+        for i in range(len(sample_cat_oh)):
+            sample_cat_oh[i] = F.one_hot(torch.multinomial(pred_softmax[i], target[i].sum(), replacement=False).squeeze(), 
+                                         num_classes=target.shape[1])
+        return sample_cat_oh
+    
     def calc_pi_acc(self, pred, target, features):
         device = next(self.parameters()).device
         pred_1 = pred  # prediction of discrimination pathway (using all filters)
+
+        # sample class ID using reparameter trick
+        # pred_softmax = torch.softmax(pred, dim=-1)
+        # with torch.no_grad():
+        #     sample_cat = torch.multinomial(pred, target.sum(1), replacement=False).flatten().cuda()
+        #     # sample_cat_oh = self.multinomial(pred, target).to(device)
+        #     ind_positive_sample = sample_cat_oh == target
+        #     sample_cat_oh = sample_cat.float().to(device)   # One-hot is not needed since it's multi-label
+        # #   ind_positive_sample = sample_cat == target  # mark wrong sample results
+        #     # sample_cat_oh = F.one_hot(sample_cat, num_classes=pred.shape[1]).float().cuda()
+        #     epsilon = torch.where(sample_cat_oh != 0, 1 - pred, -pred).detach()
+        # sample_cat_oh = pred + epsilon
+
+        # # sample filter using reparameter trick
+        # correlation_softmax = F.softmax(self.correlation, dim=0)
+        # correlation_samples = sample_cat_oh @ correlation_softmax
+        # with torch.no_grad():
+        #     ind_sample = torch.bernoulli(correlation_samples).bool()
+        #     epsilon = torch.where(ind_sample, 1 - correlation_samples, -correlation_samples)
+        # binary_mask = correlation_samples + epsilon
+        # feature_mask = features * binary_mask[..., None]  # binary
+        # pred_2 = self.classifier(feature_mask) # prediction of Interpretation pathway (using a cluster of class-specific filters)
+        # with torch.no_grad(): 
+        #     correlation_samples = correlation_softmax[target]
+        #     binary_mask = torch.bernoulli(correlation_samples).sum(1).bool()
+        #     feature_mask_self = features * ~binary_mask[..., None]
+        #     pred_3 = self.classifier(feature_mask_self) # prediction of Interpretation pathway (using complementary clusters of class-specific filters)
+        # out = {"features": features, 'pred_1': pred_1, 'pred_2': pred_2, 'pred_3': pred_3,
+        #             'ind_positive_sample': ind_positive_sample}
+        # return out
+
         correlation = nn.Parameter(F.softmax(torch.rand(size=(target.shape[-1], features.shape[1])), dim=0)).to(device)
         # sample class ID using reparameter trick (adjusted for multi-label)
         with torch.no_grad():
@@ -34,7 +72,7 @@ class LoadModel(nn.Module):
         sample_cat_oh = pred + epsilon
 
         # sample filter using reparameter trick (adjusted for multi-label)
-        correlation_softmax = F.softmax(correlation, dim=0)
+        correlation_softmax = F.softmax(correlation, dim=0) # correlation
         correlation_samples = sample_cat_oh @ correlation_softmax
         with torch.no_grad():
             ind_sample = torch.bernoulli(correlation_samples).bool()
@@ -148,6 +186,7 @@ def create_config(config_file, backbone=None, criterion=None):
 
 if __name__ == '__main__':
     import argparse
+    from MLPMixer import resnet
     cmd_opt = argparse.ArgumentParser(description='Argparser for PICNN')
     cmd_opt.add_argument('-configFileName', default='./configs/cifar10.yml')
     cmd_opt.add_argument('-criterion', default='ClassSpecificCE',help='StandardCE/ClassSpecificCE')
@@ -157,6 +196,11 @@ if __name__ == '__main__':
                       backbone=cmd_args.backbone,
                       criterion=cmd_args.criterion)   
     input = torch.randn((128, 3, 224, 224)).cuda()
-    target = F.one_hot(torch.randint(0, 10, (128,)), num_classes=10).cuda()
-    model = LoadModel(p).cuda()
+    # target = F.one_hot(torch.randint(0, 10, (128,)), num_classes=10).cuda()
+    a=np.array([[0,1,0,1,0,0,0,0,0,0,], [1,0,0,0,0,0,0,0,1,0], [0,1,1,1,1,1,0,0,0,0,],[0,0,0,0,0,0,1,0,1,1,]])
+    target = []
+    for i in range(32):
+        target.append(a)
+    target = torch.tensor(np.vstack(target)).cuda()
+    model = PIModel(p).cuda()
     model(input, target)
